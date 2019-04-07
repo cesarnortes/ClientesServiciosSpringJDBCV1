adb895675091f2ddcdd7e406d393728fc96cc621
cesarnortes <49324389+cesarnortes@users.noreply.github.com>
2019-04-07 14:26:00 +0200

    daoCliente

--- ClientesServicios/src/main/java/modelo/ClienteDAO.java
+++ ClientesServicios/src/main/java/modelo/ClienteDAO.java
@@ -12,8 +12,6 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-
-
 import dominio.Cliente;
 import dominio.Servicio;
 
@@ -33,10 +31,11 @@ public class ClienteDAO implements ClienteDAOInterface {
 //por defecto todas las clases heredan un contructor o puedes crearlo
 	///////////////////////////////////////////////////////////////
 
-	//esta variable cargara los datos necesarios en el archivo de configuracion
-	//matiendo el codigo fuente del dao lo mas independiente posible
-	//sobre el tipo de base de datos, su usuario, su password, su direccion, sea cual sea los datos de configuracion
-	//se ponen aparte para no alterar un codigo fuente de calidad
+	// esta variable cargara los datos necesarios en el archivo de configuracion
+	// matiendo el codigo fuente del dao lo mas independiente posible
+	// sobre el tipo de base de datos, su usuario, su password, su direccion, sea
+	// cual sea los datos de configuracion
+	// se ponen aparte para no alterar un codigo fuente de calidad
 	JdbcTemplate jdbcTemplate;
 
 	public JdbcTemplate getJdbctemplate() {
@@ -101,61 +100,67 @@ public class ClienteDAO implements ClienteDAOInterface {
 
 	}
 
-	 
 	public void modificacion(Cliente c) {
-		
-		String sql="update tclientes set Nombre=?, Telefono=? where IdCliente=?";
-		jdbcTemplate.update(sql,c.getNombre(),c.getTelefono(),c.getIdCliente());
-		
+
+		// se llama al mismo metodo que la alta .update pero la sentencia sql sera
+		// distinta
+		String sql = "update tclientes set Nombre=?, Telefono=? where IdCliente=?";
+		jdbcTemplate.update(sql, c.getNombre(), c.getTelefono(), c.getIdCliente());
+
 	}
 
 	// el parametro readOnly=true de la anotacion indica que el metodo
 	// solo va a leer información de la base de datos
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 	public Cliente consulta(int idCliente) {
 		// TODO Auto-generated method stub
-				String sql = "select * from tclientes WHERE IdCliente=?";
-				//en  new Object[] { idCliente } especificamos el valor del parametro de IdCliente=?
-				Cliente cliente = (Cliente) jdbcTemplate.queryForObject(sql, new Object[] { idCliente }, new ClienteMapper());
-				return cliente;
- 	}
+		String sql = "select * from tclientes WHERE IdCliente=?";
+		// en new Object[] { idCliente } especificamos el valor del parametro de
+		// IdCliente=?
+		Cliente cliente = (Cliente) jdbcTemplate.queryForObject(sql, new Object[] { idCliente }, new ClienteMapper());
+		return cliente;
+	}
 
-	 
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 	public List<Cliente> consultaAll() {
-		String sql="select * from tclientes";
-		
-		//jdbcTemplate.query devulve un objeto List<> del tipo de objeto que creas
-		List<Cliente> todosClientes= jdbcTemplate.query(sql, new ClienteMapper());
+		String sql = "select * from tclientes";
+
+		// jdbcTemplate.query devulve un objeto List<> del tipo de objeto que creas
+		List<Cliente> todosClientes = jdbcTemplate.query(sql, new ClienteMapper());
 		return todosClientes;
 	}
-	
+
 	///////////////////////////////////////////////////////////////////////
 
-	//Mapper
-	//Los mapper son la clase que hay que desarrollar donde especificamos que columna corresponde con con que variable 
-	// del objeto que queremos contruir y finalmente devuelve un objero del tipo especificado
-	// siempre que que haga una consulta hay que pasarle por parametro al metodo de consulta de jdbcTemplate
+	// Mapper
+	// Los mapper son la clase que hay que desarrollar donde especificamos que
+	// columna corresponde con con que variable
+	// del objeto que queremos contruir y finalmente devuelve un objero del tipo
+	// especificado
+	// siempre que que haga una consulta hay que pasarle por parametro al metodo de
+	// consulta de jdbcTemplate
 	// un mapper del objeto que deseas consultar
-	
+
 	class ClienteMapper implements RowMapper<Cliente> {
 		public Cliente mapRow(ResultSet rs, int rowNum) throws SQLException {
-			
-			//creamos el objeto 
+
+			// creamos el objeto
 			Cliente cliente = new Cliente();
-			
-			//asignamos el valor extraido en el resulset en sus variables con los setter
+
+			// asignamos el valor extraido en el resulset en sus variables con los setter
 			cliente.setIdCliente(rs.getInt("IdCliente"));
 			cliente.setNombre(rs.getString("Nombre"));
 			cliente.setTelefono("Telefono");
 
-			//cuado el objeto tiene un array de otro objeto u otro objeto a secas almacenado en otra tabla
-			//hay que hacer una consulta para extraer los registros necesarios
+			// cuado el objeto tiene un array de otro objeto u otro objeto a secas
+			// almacenado en otra tabla
+			// hay que hacer una consulta para extraer los registros necesarios
 			String sql = "Select * from TServicios where idCliente=?";
 			List<Servicio> servicio = jdbcTemplate.query(sql, new Object[] { cliente.getIdCliente() },
 					new ServicioMapper());
-			//como se puede ver queremos un array de los servicios del cliente entonces al crear un cliente 
-			//necesitamos cargar todos los servicios del mismo modo de siempre 
+			// como se puede ver queremos un array de los servicios del cliente entonces al
+			// crear un cliente
+			// necesitamos cargar todos los servicios del mismo modo de siempre
 
 			cliente.setListaServicios((ArrayList<Servicio>) servicio);
 
@@ -172,17 +177,20 @@ public class ClienteDAO implements ClienteDAOInterface {
 			servicio.setIdServicio(rs.getInt("IdServicio"));
 			servicio.setDescripcion(rs.getString("Concepto"));
 			servicio.setFecha(rs.getDate("Fecha"));
-			//este maper de servicio es exactamente igual solo que no necesita un array solo un cliente especifico
-			//consultamos el cliente con jdbcTemplate.queryForObject para especificar que la busqueda
-			//solo trae un objeto o registro, de la mis forma pasamos el mapper del objeto que debe crear
-			//y asi ya usaria el codigo la aplicacion de manera recurrente una y otra vez segun objetos y objetos anidados
-			//tenga que crear
+			// este maper de servicio es exactamente igual solo que no necesita un array
+			// solo un cliente especifico
+			// consultamos el cliente con jdbcTemplate.queryForObject para especificar que
+			// la busqueda
+			// solo trae un objeto o registro, de la mis forma pasamos el mapper del objeto
+			// que debe crear
+			// y asi ya usaria el codigo la aplicacion de manera recurrente una y otra vez
+			// segun objetos y objetos anidados
+			// tenga que crear
 			servicio.setCliente(jdbcTemplate.queryForObject(sql, new Object[] { idCliente }, new ClienteMapper()));
 
 			return servicio;
 
 		}
 	}
-
 
 }
